7aff99245ee9f8d0eed5a3959ab7a3cbe10bd848
valpaulo <val.otoman@gmail.com>
2024-08-12 12:48:11 +0800

    get recipes per category

--- filipinorecipes/filipinorecipes/spiders/recipespider.py
+++ filipinorecipes/filipinorecipes/spiders/recipespider.py
@@ -33,10 +33,10 @@ class RecipespiderSpider(scrapy.Spider):
                 })
 
                 if link:
-                    yield scrapy.Request(response.urljoin(link), callback=self.parse_subcategory, meta={'category': category, 'subcategory': subcategory_name})
-                
+                    yield response.follow(link, callback=self.parse_subcategory, meta={'category': category, 'subcategory': subcategory_name})
+
         df = pd.DataFrame(data)
-        # df.to_csv("categories_and_subcategories.csv", index=False)
+        df.to_csv("categories_and_subcategories.csv", index=False)
         print(df)
 
         connection_string = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
@@ -47,5 +47,29 @@ class RecipespiderSpider(scrapy.Spider):
     def parse_subcategory(self, response):
         category = response.meta['category']
         subcategory = response.meta['subcategory']
+        recipe_links = response.css("a.entry-title-link::attr(href)").getall()
+        recipe_titles = response.css("a.entry-title-link::text").getall()
 
-        pass
+
+        # Collect subcategory data
+        subcategory_data = []
+        for title, link in zip(recipe_titles, recipe_links):
+            subcategory_data.append({
+                "Category": category,
+                "Subcategory": subcategory,
+                "Recipe": title,
+                "Recipe Link": link
+            })
+
+        # Create a DataFrame for subcategory data
+        subcategory_df = pd.DataFrame(subcategory_data)
+
+        # Save subcategory data to CSV
+        subcategory_df.to_csv(f"recipes_{category}_{subcategory}.csv", index=False)
+        print(subcategory_df)
+
+        next_page  = response.css("li.pagination-next a::attr(href)").get()
+        
+        if next_page:
+            yield response.follow(next_page, callback=self.parse_subcategory, meta={'category': category, 'subcategory': subcategory})
+        
